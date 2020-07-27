22595545168c3f5045f007b880bec11599d1b3ee
Kiru Park <kirumang@gmail.com>
2020-07-27 17:45:31 +0200

    updated the example script for rendering

--- examples/NOL_rendering.py
+++ examples/NOL_rendering.py
@@ -1,58 +1,71 @@
+'''
+Example code that renders objects from uniform viewpoints in the upper-hemisphere.
+The number of result iamges is 1296, which are used to train object recognizers in the evaluation
+The NOL data file (.hdf5) must be created using one of the scripts in the "data_processing" folder.
+'''
 import os,sys
 import numpy as np
 import cv2
 sys.path.append(os.path.abspath('.'))  
+import tensorflow as tf
 tf.autograph.set_verbosity(0)
 
 from NOL_model import NOL
+from matplotlib import pyplot as plt
+import json
 
-'''
-Todo: replace im_w,im_h, with a config file
-Arg1: config.json
-Arg2: src_fn
+if(len(sys.argv)<3):
+    print("usage: python3 examples/NOL_rendering.py [path/to/camera_cfg(.json)] [path/to/NOL_data(.hdf)] [(optional)target_dir]")
+    sys.exit()
 
-'''
-
-#src_fn,im_h,im_w,cam_K):  
-dataset="ho3d"
-if(dataset=="smot"):
-    cam_K = np.array([[538.391033533567,0,315.3074696331638],
-                      [0,538.085452058436,233.0483557773859],
-                      [0,0,1]])
-    fx = cam_K[0,0]
-    fy = cam_K[1,1]
-    cx = cam_K[0,2]
-    cy = cam_K[1,2]
-    im_h=480
-    im_w=640
-
+camera_cfg_fn=sys.argv[1]
+src_fn = sys.argv[2]
+if(len(sys.argv)==4):
+    target_dir = sys.argv[3]
 else:
-    im_w = 640
-    im_h = 480
-    cam_K=np.array([[614.0,   0.   , 639/2],
-                     [  0.   , 614.0, 479/2],
-                     [  0.   ,   0.   ,   1. ]])
-
-src_fn = "./sample_data/ho3d/010_potted_meat_can.hdf5"
-m_NOL = NOL.NOL(src_fn,im_h=im_h,im_w=im_w,cam_K=cam_K)
-
-
-result_dir = "./results"
-if not(os.path.exists(result_dir)):os.makedirs(result_dir)
+    #default target : ./results/[data file name]/"
+    filename= src_fn.split('/')[-1]
+    filename = filename.replace(".hdf5","")    
+    target_dir = "./results/{}".format(filename)
+if not(os.path.exists(target_dir)): os.makedirs(target_dir)
     
+with open(camera_cfg_fn, 'r') as f:
+        camera_intrinsic = json.load(f)
+cam_K=np.array(camera_intrinsic['cam_K']).reshape(3,3)
+fx = cam_K[0,0]
+fy = cam_K[1,1]
+cx = cam_K[0,2]
+cy = cam_K[1,2]
+im_width = camera_intrinsic['width']
+im_height = camera_intrinsic['height']
+
+
+m_NOL = NOL.NOL(src_fn,im_h=im_height,im_w=im_width,cam_K=cam_K)
+
+
 update_rate=1E-5 #for LineMOD and SMOT
 pose_list = np.load("./sample_data/pose_target_lm.npy")
 total_ids = np.arange(np.arange(0,360,5).shape[0]*np.arange(0,90,5).shape[0])
 azi_ele = total_ids.reshape(72,18)
 ele_azi = np.transpose(azi_ele).reshape(-1)
 img_cnt=0
-for elaz_id in np.arange(0,ele_azi.shape[0],3):#[target_pose_id]:
+
+print("Start rendering the object...")
+
+
+max_iter=50 #Decrease max_iter to increase the speed
+back_color=[0,0,0] #background color (default:black, white:[1,1,1])
+print("[Info] Decrease max_iter to increase the speed, current value:",max_iter)
+for elaz_id in np.arange(0,ele_azi.shape[0],3):
     pose_id = ele_azi[elaz_id]
     pose = pose_list[pose_id]
     target_pose=np.eye(4)
     target_pose[:3,:3] =pose[:3,:3]
     target_pose[:3,3]=[0,0,0.7]
-    img = m_NOL.render_a_pose(target_pose,max_iter=50,update_rate=update_rate) #done
+    img = m_NOL.render_a_pose(target_pose,max_iter=max_iter,update_rate=update_rate,back_color=[0,0,0]) #done
     img_cv2 = np.clip((img[:,:,::-1]*255).astype(np.uint8),0,255)
-    cv2.imwrite(os.path.join(result_dir,"{:04d}.png".format(img_cnt)),img_cv2)
-    img_cnt+=1
+    cv2.imwrite(os.path.join(target_dir,"{:04d}.png".format(img_cnt)),img_cv2)
+    print("Generated:",os.path.join(target_dir,"{:04d}.png".format(img_cnt)))
+    img_cnt+=1    
+    if(img_cnt%100==0):
+        print("[Info] Decrease max_iter to increase the speed, current value:",max_iter)
